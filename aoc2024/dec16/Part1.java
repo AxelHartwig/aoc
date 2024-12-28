1df3578bdbae4783be61644303d453e0c837efd0
AxelHartwig <axel@hartwig.nu>
2024-12-28 03:00:52 +0100

    16del2

--- aoc2024/dec16/Part1.java
+++ aoc2024/dec16/Part1.java
@@ -3,206 +3,146 @@ import java.util.*;
 class Part1 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        ArrayList<Node> positions = new ArrayList<>();
+        int size = 141;
+        Node[][][] nodes = new Node[size][size][4];
+        char[][] m = new char[size][size];
         int cnt = 0;
-        char[][] m = new char[15][15];
         while (scan.hasNextLine()) {
-            String line = scan.nextLine();
-            m[cnt] = line.toCharArray();
-            for (int j = 0; j < line.length(); j++) {
-                char c = line.charAt(j);
-                for (int i = 0; i < 4; i++) {
-                    positions.add(new Node(c, j, cnt, i));
-                }
-            }
+            m[cnt] = scan.nextLine().toCharArray();
             cnt++;
         }
 
-        ArrayList<Node> positions1 = new ArrayList<>();
-        ArrayList<Node> positions2 = new ArrayList<>();
-        ArrayList<Node> positions3 = new ArrayList<>();
-        ArrayList<Node> positions4 = new ArrayList<>();
-
-        for (Node n : positions) {
-            positions1.add(new Node(n.c, n.x, n.y, n.direction));
-            positions2.add(new Node(n.c, n.x, n.y, n.direction));
-            positions3.add(new Node(n.c, n.x, n.y, n.direction));
-            positions4.add(new Node(n.c, n.x, n.y, n.direction));
-        }
-
-        PriorityQueue<Node> q = new PriorityQueue<>();
-
-        djikstra(positions, 'S', 1, m);
-        int part1 = Integer.MAX_VALUE;
-        for (Node n : positions) {
-            if (n.c == 'E') {
-                // System.out.println(n.dist);
-                if (n.dist < part1) {
-                    part1 = n.dist;
-                }
-            }
-        }
-        cnt = 0;
+        int startR = 0;
+        int startC = 0;
+        int endR = 0;
+        int endC = 0;
         for (int i = 0; i < m.length; i++) {
             for (int j = 0; j < m.length; j++) {
-                System.out.print(m[i][j]);
-            }
-            System.out.println();
-        }
-        System.out.println(part1);
+                for (int k = 0; k < 4; k++) {
+                    nodes[i][j][k] = new Node(m[i][j], j, i, k);
+                }
 
+                if (m[i][j] == 'S') {
+                    startR = i;
+                    startC = j;
+                }
+
+                if (m[i][j] == 'E') {
+                    endR = i;
+                    endC = j;
+                }
+            }
+        }
+
+        Queue<Node> q = new PriorityQueue<>();
+        HashSet<Node> paths = new HashSet<>();
+        Node start = nodes[startR][startC][1];
+        start.dist = 0;
+        // start.parent.add()
+        q.add(start);
         // 0 = N, 1 = E, 2 = S, 3 = W
-
-    }
-
-    private static void djikstra(ArrayList<Node> positions, char start, int direction, char[][] m) {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-
-        for (Node n : positions) {
-            if (n.c == start && n.direction == direction) {
-                n.visited = true;
-                n.dist = 0;
-                q.add(n);
-            }
-        }
-
+        int[][] dirs = { { 0, -1 }, { 1, 0 }, { 0, 1 }, { -1, 0 } };
+        int lowestscore = -1;
         while (!q.isEmpty()) {
             Node curr = q.poll();
-            m[curr.y][curr.x] = 'O';
-            for (Node n : turns(curr, positions)) {
-                int newdist = curr.dist + 1000;
-                if (!n.visited && n.c != '#' && newdist < n.dist) {
-                    n.dist = newdist;
-                    n.visited = true;
-                    n.prev = curr;
-                    q.add(n);
+
+            // walk straight
+            int nextx = curr.x + dirs[curr.direction][0];
+            int nexty = curr.y + dirs[curr.direction][1];
+            if (nextx >= size || nextx < 0 || nexty >= size || nexty < 0) {
+                continue;
+            }
+            int newdist = 0;
+            if (m[nexty][nextx] != '#') {
+                newdist = curr.dist + 1;
+                Node walk = nodes[nexty][nextx][curr.direction];
+                if (newdist < walk.dist) {
+                    q.add(walk);
+                    walk.parent.clear();
+                    walk.parent.add(curr);
+                    walk.dist = newdist;
+                } else if (newdist == walk.dist) {
+                    walk.parent.add(curr);
                 }
             }
 
-            Node n = neighbour(curr, positions);
-            if (n == null) {
-                continue;
+            // turn cost
+            newdist = curr.dist + 1000;
+
+            // Turn right
+            Node right = nodes[curr.y][curr.x][Math.floorMod((curr.direction + 1), 4)];
+            if (newdist < right.dist) {
+                q.add(right);
+                right.parent.clear();
+                right.parent.add(curr);
+                right.dist = newdist;
+            } else if (newdist == right.dist) {
+                right.parent.add(curr);
             }
 
-            int newdist = curr.dist + 1;
-            if (!n.visited && n.c != '#' && newdist < n.dist) {
-                n.dist = newdist;
-                n.visited = true;
-                n.prev = curr;
-                q.add(n);
-            }
-
-        }
-
-    }
-
-    static Set<Node> part2(Node curr, Node start, Node end, Set<Node> path) {
-        path.add(curr);
-        if (curr.equals(start)) {
-            return path;
-        } else {
-            ArrayList<Node> next = new ArrayList<>();
-        }
-    }
-
-    static ArrayList<Node> turns(Node n, ArrayList<Node> positions) {
-        ArrayList<Node> turns = new ArrayList<>();
-        for (Node n1 : positions) {
-            if (n1.x == n.x && n1.y == n.y) {
-                switch (n.direction) {
-                    case 0:
-                        if (n1.direction == 1 || n1.direction == 3) {
-                            turns.add(n1);
-                        }
-                        break;
-                    case 1:
-                        if (n1.direction == 0 || n1.direction == 2) {
-                            turns.add(n1);
-                        }
-                        break;
-                    case 2:
-                        if (n1.direction == 1 || n1.direction == 3) {
-                            turns.add(n1);
-                        }
-                        break;
-                    case 3:
-                        if (n1.direction == 0 || n1.direction == 2) {
-                            turns.add(n1);
-                        }
-                        break;
-                }
+            // Turn left
+            Node left = nodes[curr.y][curr.x][Math.floorMod((curr.direction - 1), 4)];
+            if (newdist < left.dist) {
+                q.add(left);
+                left.parent.clear();
+                left.parent.add(curr);
+                left.dist = newdist;
+            } else if (newdist == left.dist) {
+                left.parent.add(curr);
             }
         }
 
-        return turns;
-    }
-
-    static Node neighbour(Node n, ArrayList<Node> positions) {
-        for (Node n1 : positions) {
-            if (n1.direction != n.direction) {
-                continue;
-            }
-            switch (n.direction) {
-                case 0:
-                    if (n.x == n1.x && n.y - 1 == n1.y) {
-                        return n1;
-                    }
-                    break;
-                case 1:
-                    if (n.x + 1 == n1.x && n.y == n1.y) {
-                        return n1;
-                    }
-                    break;
-                case 2:
-                    if (n.x == n1.x && n.y + 1 == n1.y) {
-                        return n1;
-                    }
-                    break;
-                case 3:
-                    if (n.x - 1 == n1.x && n.y == n1.y) {
-                        return n1;
-                    }
-                    break;
+        int part1 = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; i++) {
+            if (part1 > nodes[1][size - 2][i].dist) {
+                part1 = nodes[1][size - 2][i].dist;
             }
         }
-        return null;
+
+        int part2 = find_paths(nodes[1][size - 2][3], new HashSet<>(), new HashSet<>());
+
+        System.out.println(part1);
+        System.out.println(part2);
+
     }
 
+    static int find_paths(Node curr, HashSet<Node> nodes, HashSet<Node> path) {
+        if (curr.parent.size() == 0) {
+            nodes.addAll(path);
+        }
+
+        for (Node parentNode : curr.parent) {
+            path.add(curr);
+
+            find_paths(parentNode, nodes, path);
+
+            path.remove(curr);
+        }
+
+        return nodes.size();
+    }
 }
 
 class Node implements Comparable<Node> {
     char c;
     int x;
     int y;
-    int dist;
-    boolean visited;
     int direction;
-    Node prev;
+    int dist = Integer.MAX_VALUE;
+    ArrayList<Node> parent = new ArrayList<>();
 
     public Node(char c, int x, int y, int direction) {
-        dist = Integer.MAX_VALUE;
-        visited = false;
         this.c = c;
         this.x = x;
         this.y = y;
         this.direction = direction;
     }
 
-    // public boolean equals(Object obj) {
-    // if (!(obj instanceof Node other)) {
-    // return false;
-    // }
-    // return this.visited == other.visited && other.c == this.c && this.x ==
-    // other.x && this.y == other.y
-    // && this.direction == other.direction && this.dist == other.dist;
-    // }
-
     @Override
     public boolean equals(Object obj) {
         if (!(obj instanceof Node other)) {
             return false;
         }
-
         return this.x == other.x && this.y == other.y;
     }
 
@@ -215,4 +155,5 @@ class Node implements Comparable<Node> {
     public int compareTo(Node o) {
         return this.dist - o.dist;
     }
+
 }
